9dfe44ef1d0d1bc94347db6876e2fd45bc7033a9
Lalla Sankara <lallasankara@gmail.com>
2024-03-25 20:18:54 -0700

    Update 01_data_types.py
    answers to class one

--- class_one/01_data_types.py
+++ class_one/01_data_types.py
@@ -4,7 +4,7 @@ import numbers
 
 # Your First Program
 #
-# print('hello world')
+ print('hello world')
 #
 # # Comments in python use a '#'
 # # Remove the # in front of code to run it
@@ -40,7 +40,7 @@ f = list(range(0, 10))
 
 # What is type(e)?
 
-# list
+# Its the data type list
 
 # What is len(e)?
 
@@ -48,24 +48,24 @@ f = list(range(0, 10))
 
 # What is type(f)?
 
-#list
+#Its the data type list
 
 # What are the contents of f?
 
-# contents are [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ]
+# The contents are [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ]
 
 # What is 'list' called? # hint: google it
 
-# A list is simply called list
-
-# A list is used to store collections of items, where each item in the list can be of any data type, and the items are
+# A list is simply called list in python. A list is used to store collections of items, where each item in the list can be of any data type, and the items are
 # ordered and mutable.
 
 # What is 'range' called? What happens when you run 'help(range)'?
 
-# In python range is a built in fuction. It's used to generate a sequence of numbers.
+# In python range is a built in function. It's used to generate a sequence of numbers.
 # When you run help(range) in Python, it displays information about the range function, including its usage,
 # parameters, and return value.
 
-help(range)
+## Results from running help (range)
 
+f = list(range(0, 10))
+help (range)
